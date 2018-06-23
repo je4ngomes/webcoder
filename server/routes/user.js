57f179eb9f2d5add23720d337a769a98eefaf064
Jean <38874366+n1ghtw0lff@users.noreply.github.com>
2018-06-23 14:25:50 -0300

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,4 @@ route.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../../client/user/login.html'));
 });
 
-route.post('/login', (req, res, next) => {});
-
-
 module.exports = route;
